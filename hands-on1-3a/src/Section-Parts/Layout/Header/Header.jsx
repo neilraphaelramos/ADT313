034c80713d641625610d2b2f284d27a475b9df2b
neilraphaelramos <neilraphael.ramos4@gmail.com>
2024-08-26 19:19:00 +0800

    hands on 1 Updated

---
+++ hands-on1-3a/src/Section-Parts/Layout/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Neil Raphael Ramos</h1>
+            <nav>
+                <ul>
+                    <li><a href="#about">About</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
